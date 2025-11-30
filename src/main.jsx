72d376a76370f5ab25addeeed62759ad827f078a
LeThienDat232 <lethiendat2005@gmail.com>
2025-12-01 01:56:40 +0700

    style: add comments and formatting in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react' // a special wrapper used only in development to: warn about unsafe patterns, run some functions twice (in dev) to catch side-effects.
+import { createRoot } from 'react-dom/client' //connect React to the HTML page.
 import './index.css'
-import App from './App.jsx'
-import ErrorBoundary from './components/ErrorBoundary.jsx'
+import App from './App.jsx' // imports root component.
+import ErrorBoundary from './components/ErrorBoundary.jsx' //imports a custom component to catch errors in the component tree.
+
+// Render the App component wrapped in StrictMode and ErrorBoundary to the DOM element with id 'root'.
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,3 +13,4 @@ createRoot(document.getElementById('root')).render(
     </ErrorBoundary>
   </StrictMode>,
 )
+
